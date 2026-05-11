b1e7c5dac181db318fb3900c70fd4df98efbb466
Shoab <ds.shoab@gmail.com>
2026-05-11 16:58:30 +0000

    fix: add missing </div> closing tag for row wrapper in assessments page

--- scopesnap-web/app/(app)/assessments/page.tsx
+++ scopesnap-web/app/(app)/assessments/page.tsx
@@ -258,6 +258,7 @@ export default function EstimatesPage() {
                       <polyline points="3 6 5 6 21 6"/><path d="M19 6l-1 14a2 2 0 01-2 2H8a2 2 0 01-2-2L5 6"/><path d="M10 11v6M14 11v6"/><path d="M9 6V4a1 1 0 011-1h4a1 1 0 011 1v2"/>
                     </svg>
                   </button>
+                </div>
               );
             })}
           </div>
